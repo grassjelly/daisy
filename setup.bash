648e6b0a95a6bf0dfd7b57a9c043e9eafa39403f
Juan Jimeno <jimenojmm@gmail.com>
2023-09-10 04:02:18 +0800

    added export

--- setup.bash
+++ setup.bash
@@ -3,10 +3,11 @@ export ROS2_WS_MOUNT="$PWD"
 export ROS2_WS_CONTAINER_NAME="$(basename $ROS2_WS_MOUNT)"
 export DAISY_PATH=$ROS2_WS_MOUNT/daisy
 . $DAISY_PATH/.env
-alias daisy-build="$DAISY_PATH/./build"
-alias daisy-exec="$DAISY_PATH/./exec"
-alias daisy-shell="$DAISY_PATH/./shell"
-alias daisy-stop="$DAISY_PATH/./stop"
-alias daisy-gitignore="$DAISY_PATH/./gitignore"
-alias daisy-template="$DAISY_PATH/./template"
+alias daisy-build="$DAISY_PATH/bin/./build"
+alias daisy-exec="$DAISY_PATH/bin/./exec"
+alias daisy-shell="$DAISY_PATH/bin/./shell"
+alias daisy-stop="$DAISY_PATH/bin/./stop"
+alias daisy-gitignore="$DAISY_PATH/bin/./gitignore"
+alias daisy-template="$DAISY_PATH/bin/./template"
+alias daisy-export="$DAISY_PATH/bin/./export"
 unset ROS_DISTRO
