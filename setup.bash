14192a0dbc93c5d7d4dcbb1d3561fc5d8db88833
Juan Miguel Jimeno <jimenojmm@gmail.com>
2023-10-19 17:36:32 +0800

    added tmux

--- setup.bash
+++ setup.bash
@@ -1,5 +1,7 @@
-export DOCKER_FILES_PATH="$PWD/daisy/custom"
-export ROS2_WS_MOUNT="$PWD"
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SCRIPT_DIR=$(dirname $SCRIPT_DIR)
+export DOCKER_FILES_PATH="$SCRIPT_DIR/daisy/custom"
+export ROS2_WS_MOUNT="$SCRIPT_DIR"
 export ROS2_WS_CONTAINER_NAME="$(basename $ROS2_WS_MOUNT)"
 export DAISY_PATH=$ROS2_WS_MOUNT/daisy
 . $DAISY_PATH/.env
@@ -11,4 +13,5 @@ alias daisy-stop="$DAISY_PATH/bin/./stop"
 alias daisy-gitignore="$DAISY_PATH/bin/./gitignore"
 alias daisy-template="$DAISY_PATH/bin/./template"
 alias daisy-export="$DAISY_PATH/bin/./export"
+alias daisy-tmux="$DAISY_PATH/bin/./tmux"
 unset ROS_DISTRO
