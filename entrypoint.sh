c1a9d12decdefc36029e75b6071ef8e2dfa743a9
Juan Miguel Jimeno <jimenojmm@gmail.com>
2023-10-20 15:48:07 +0800

    added check if install folder exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ source /opt/ros/rosdistro/setup.bash
 # keep track of any errors from last build 
 SOURCE_ERROR=$(source /home/username/workspace/install/setup.bash  2>&1 )
 echo "$SOURCE_ERROR" > /home/username/workspace/.last_build_errors
-if [[ "$(wc -w < /home/username/workspace/.last_build_errors)" == "0" ]]; then
+if [[ "$(wc -w < /home/username/workspace/.last_build_errors)" == "0" && -d "/home/username/workspace/install" ]]; then
     # source the workspace if no errors
     source /home/username/workspace/install/setup.bash
 fi
