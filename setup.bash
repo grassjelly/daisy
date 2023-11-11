34fb46142b153ec954e199e1fd9a48204eb40244
Juan Miguel Jimeno <jimenojmm@gmail.com>
2023-11-12 00:37:02 +0800

    added gpu and runtime vars

--- setup.bash
+++ setup.bash
@@ -1,14 +1,30 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 SCRIPT_DIR=$(dirname $SCRIPT_DIR)
+. $DAISY_PATH/.env
+
+FOXY_UBUNTU=18.04
+GALACTIC_UBUNTU=20.04
+HUMBLE_UBUNTU=22.04
+IRON_UBUNTU=22.04
+UBUNTU_VER="${USE_ROS_DISTRO^^}_UBUNTU"
+export UBUNTU_VER=${!UBUNTU_VER}
+
+GPU_TRUE="gpu"
+GPU_FALSE=""
+GPU_=""
+GPU_BASE="GPU_${ENABLE_GPU^^}"
+export GPU_BASE=${!GPU_BASE}
+
+NVIDIA_RUNTIME_TRUE="nvidia"
+NVIDIA_RUNTIME_FALSE="dockerd"
+DOCKER_RUNTIME="NVIDIA_RUNTIME_${ENABLE_GPU^^}"
+export DOCKER_RUNTIME=${!DOCKER_RUNTIME}
+
 export DOCKER_FILES_PATH="$SCRIPT_DIR/daisy/custom"
 export ROS2_WS_MOUNT="$SCRIPT_DIR"
 export ROS2_WS_CONTAINER_NAME="$(basename $ROS2_WS_MOUNT)"
 export DAISY_PATH=$ROS2_WS_MOUNT/daisy
 export U_ID=$(id -u)
-if [ -d "/sys/module/nvidia" ]; then
-    export NVIDIA_DRIVER=$(cat /sys/module/nvidia/version)
-fi
-. $DAISY_PATH/.env
 alias daisy-build="$DAISY_PATH/bin/./build"
 alias daisy-exec="$DAISY_PATH/bin/./exec"
 alias daisy-compose="$DAISY_PATH/bin/./compose"
